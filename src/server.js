e9ae871272814fc0d4cbc53ea83588f0e8b917fb
Jose Luis Campaña <macpanda@gmail.com>
2020-11-05 17:28:04 +0000

    changes?

--- src/server.js
+++ src/server.js
@@ -4,6 +4,6 @@ const { PORT: port = 3000 } = process.env;
 console.log(`listening at http://localhost:${port}`);
 //create a server object:
 http.createServer(function (req, res) {
-  res.write(`Hola fistro, te da cuen. Ésta es la respuesta del server en el puerto ${port}`); //write a response to the client
+  res.write(`Hola fistro, te da cuen. Esta es la respuesta del server en el puerto ${port}`); //write a response to the client
   res.end(); //end the response
 }).listen(Number(port));
