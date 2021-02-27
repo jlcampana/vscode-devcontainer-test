d118a40699975f38ede286b2eb00190f9e60d878
Jose Luis Campaña <macpanda@gmail.com>
2021-02-27 23:44:56 +0100

    ready

--- src/server.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const { PORT: port = 3000 } = process.env;
-
-console.log(`listening at http://localhost:${port}`);
-//create a server object:
-http.createServer(function (req, res) {
-  res.write(`Hola fistro, te da cuen. Esta es la respuesta del server en el puerto ${port}`); //write a response to the client
-  res.end(); //end the response
-}).listen(Number(port));
