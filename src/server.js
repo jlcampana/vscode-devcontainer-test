b4b1cd21b1034cfb8f1ef7d253d7c2221c275970
Jose Luis Campaña <macpanda@gmail.com>
2023-09-23 10:00:20 +0200

    Print Node and OS ver

--- src/server.js
+++ src/server.js
@@ -1,9 +1,13 @@
 const http = require('http');
 const { PORT: port = 3000 } = process.env;
+const { platform, release } = require('os');
+const versionOS = `${platform()} ${release()}`
 
 console.log(`listening at http://localhost:${port}`);
 //create a server object:
 http.createServer(function (req, res) {
   res.write(`Hola fistro, te da cuen. Esta es la respuesta del server en el puerto ${port}`); //write a response to the client
+  res.write(`\nOS: ${versionOS}`);
+  res.write(`\nNode ver: ${process.versions.node}`);
   res.end(); //end the response
 }).listen(Number(port));
